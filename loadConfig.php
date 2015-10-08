e116bd163c4e881894a48921601a4a99b253437f
Becker Ewing <jbewing@taftschool.org>
2015-10-07 23:15:42 -0400

    Started PHP backend

---
+++ loadConfig.php
@@ -0,0 +1,21 @@
+<?php
+    function loadConfig() {
+        $db_array = parse_ini_file("config.ini");
+        $GLOBALS['db_server_name'] = $db_array['ipad'] . ':' . $db_array['port'];
+        $GLOBALS['db_username'] = $db_array['user'];
+        $GLOBALS['db_password'] = $db_array['pass'];
+        $GLOBALS['db_dbname'] = $db_array['name'];
+    }
+
+    function getSQLConnection() {
+        $conn = new mysqli($GLOBALS['db_server_name'], $GLOBALS['db_username'],
+                           $GLOBALS['db_password']);
+
+        if($conn->connect_error) {
+            die("Connection failed: ". $conn->connect_error);
+        }
+        echo "Connected Successfully";
+    }
+    loadConfig();
+    getSQLConnection();
+?>
