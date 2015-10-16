22e458da3a4f844a65649fe37de5210616ebb03d
Becker Ewing <jbewing@taftschool.org>
2015-10-16 11:48:35 -0400

    Simplified club_searcher SQL statement immensely

--- scripts/clubs_searcher.php
+++ scripts/clubs_searcher.php
@@ -9,40 +9,20 @@ if(isset($_GET['a'])) {
                 $conn = getSQLConnectionFromConfig();
                 $result = "";
                 if($value == 'All') {
-                    $result = $conn->query('SELECT uClub.name as name, uClub.mission_statement as mission, uClub.preferred_name as leader_first, uClub.last_name as leader_last, advisor.preferred_name as advisor_first, advisor.last_name as advisor_last
-FROM sgstudents.seniors_data as advisor
-INNER JOIN	(SELECT aClub.name, aClub.mission_statement, aClub.advisor, people.preferred_name, people.last_name
-FROM sgstudents.seniors_data as people
-INNER JOIN (SELECT theClub.name, theClub.mission_statement, theClub.advisor, joiner.userId
-    FROM taftclubs.club as theClub
-    LEFT JOIN taftclubs.clubjoiners as joiner
-    ON joiner.clubId = theClub.id
-    WHERE joiner.hasLeft = 0 AND joiner.isLeader = 1) as aClub
-ON aClub.userId = people.id) as uClub
-ON uClub.advisor = advisor.id');
+
+                    $result = $conn->query('SELECT c.name as name, c.mission_statement as mission, leader.preferred_name as leader_first, leader.last_name as leader_last, advisor.preferred_name as advisor_first, advisor.last_name as advisor_last
+                                        FROM taftclubs.club c join sgstudents.seniors_data advisor on c.advisor = advisor.id
+                                        join taftclubs.clubjoiners j on c.id = j.clubId
+                                        join sgstudents.seniors_data leader on leader.id = j.userId
+                                        where j.hasLeft = 0 and j.isLeader = 1');
+
                 } else {
-                $result = $conn->query('SELECT uClub.name as name, uClub.mission_statement as mission, uClub.preferred_name as leader_first, uClub.last_name as leader_last, advisor.preferred_name as advisor_first, advisor.last_name as advisor_last
-FROM sgstudents.seniors_data as advisor
-INNER JOIN	(
-	SELECT aClub.name, aClub.mission_statement, aClub.advisor, people.preferred_name, people.last_name
-	FROM sgstudents.seniors_data as people
-	INNER JOIN (
-        SELECT theClub.name, theClub.mission_statement, theClub.advisor, joiner.userId
-		FROM taftclubs.clubjoiners as joiner
-		INNER JOIN (
-			SELECT club.name, club.mission_statement, club.advisor, clubcategories.data, club.id
-            FROM taftclubs.club
-            INNER JOIN taftclubs.clubcategories
-            ON club.category = clubcategories.id
-            WHERE clubcategories.data =' . '"' . $value . '"' . '
-            ) as theClub
-		ON joiner.clubId = theClub.id
-        WHERE joiner.hasLeft = 0 AND joiner.isLeader = 1
-        ) as aClub
-	ON aClub.userId = people.id
-    ) as uClub
-ON uClub.advisor = advisor.id
-');
+                    $result = $conn->query("SELECT c.name as name, c.mission_statement as mission, leader.preferred_name as leader_first, leader.last_name as leader_last, advisor.preferred_name as advisor_first, advisor.last_name as advisor_last
+                                        FROM taftclubs.club c join sgstudents.seniors_data advisor on c.advisor = advisor.id
+                                        join taftclubs.clubjoiners j on c.id = j.clubId
+                                        join sgstudents.seniors_data leader on leader.id = j.userId
+                                        join taftclubs.clubcategories category on c.category = category.id
+                                        where j.hasLeft = 0 and j.isLeader = 1 and category.data = '$value'");
                 }
                 if($result->num_rows > 0) {
                     while($item = $result->fetch_assoc()) {
