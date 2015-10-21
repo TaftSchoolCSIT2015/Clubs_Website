aef932811ccb3b1d3b611660cca5d85647b8f969
Becker Ewing <jbewing@taftschool.org>
2015-10-20 22:39:50 -0400

    Changed styling of registration to match that of home page

--- registration.php
+++ registration.php
@@ -15,31 +15,33 @@ $conn = getSQLConnectionFromConfig();
     <head>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" href="style/common.css">
       <link rel="stylesheet" type="text/css" href="stylesheet2.css">
       <script src="js/jquery-2.1.4.min.js"></script>
     </head>
     <body>
-      <div id="top_bar">
-
-        <a href="index.php">
-            <div id="home_logo">
-            </div>
-        </a>
-
-        <div id="title">
-          <a> Club Registration </a>
-        </div>
-
-        <div id="menu_bar">
-          <span>
-            <?php
-                getInputToLoginMenu($conn);
-            ?>
-          </span>
-        </div>
-
+      <div class="header">
+          <div class="top_bar">
+              <div class="title"><span>Club Registration</span></div>
+          </div>
+          <div class="nav">
+              <ul>
+                  <a href="index.php"><li>Home</li></a>
+                  <a href="resources/How_to_write_a_mission_statement.docx"><li>How to Write a Mission Statement</li></a>
+                  <a class="login_nav_bar"><li>
+                      <?php
+                          getInputToLoginMenu($conn);
+                      ?>
+                  </li>
+                      <ul class="login_menu_hoverable">
+                          <li>My Clubs</li>
+                          <li>Make A New Club</li>
+                          <li>Log Out</li>
+                      </ul>
+                  </a>
+              </ul>
+          </div>
       </div>
-
       <div id="restofpage">
         <div id="main_body">
           <div class= "text_line">
@@ -50,15 +52,15 @@ $conn = getSQLConnectionFromConfig();
 
           <div id="leaders_text_line">
                <form id="leaders_form" >Club Leaders:
-                 <input  id="add_leader_text" name="add_leader" type="text" >
-                 <input id="add_button" name="add_button" type="button" Value="Add Leader" />
+                 <input  id="add_leader_text" name="add_leader" type="text">
+                 <input id="add_button" name="add_button" type="button" Value="Add Leader">
                </form>
 
                <ul>
                     <?php
                         if(isset($_SESSION['name'])) {
                     ?>
-                    <li><?php echo $_SESSION['name']?><input class="X_button" type="button" Value="X" disabled /></li>
+                    <li><?php echo $_SESSION['name']?><input class="X_button" type="button" Value="X" disabled></li>
                     <?php } ?>
                </ul>
           </div>
@@ -111,23 +113,23 @@ $conn = getSQLConnectionFromConfig();
              <table>
                  <tr>
                    <td> <form class="event_form" >Title:
-                     <input id="event_title" class="event_box" type="text" >
+                     <input id="event_title" class="event_box" type="text">
                    </form>
                    </td>
                    <td> <form class="event_form" >Location:
-                     <input id="event_loc" class="event_box" type="text" >
+                     <input id="event_loc" class="event_box" type="text">
                    </form>
                    </td>
                    <td> <form class="event_form" >Date:
-                     <input id="event_date" class="event_box" type="date" >
+                     <input id="event_date" class="event_box" type="date">
                    </form>
                    </td>
                    <td> <form class="event_form" >Time:
-                     <input id="event_time" class="event_box" type="text" >
+                     <input id="event_time" class="event_box" type="text">
                    </form>
                    </td>
                    <td> <form>
-                     <input id="add_event_button" type="button" Value = "Add" />
+                     <input id="add_event_button" type="button" Value = "Add">
                    </form>
                    </td>
                  </tr>
