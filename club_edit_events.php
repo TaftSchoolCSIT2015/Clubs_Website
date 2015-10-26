ed92830a40655b3cbc5272c127536ccff4912675
Becker Ewing <jbewing@taftschool.org>
2015-10-26 01:12:01 -0400

    Added club_edit_events and club_edit_feed tabs; non-functional javascript up to this point

---
+++ club_edit_events.php
@@ -0,0 +1,82 @@
+<?php session_start(); ?>
+<?php
+if(!isset($_SESSION['user']) && !isset($_GET['club'])) { //Need to be authenticated to get to this page
+    header("Location: index.php");
+    exit();
+}
+
+$clubname = $_GET['club'];
+$username = $_SESSION['user'];
+
+require 'scripts/SQLUtils.php';
+require 'scripts/index_utils.php';
+require 'scripts/club_utils.php';
+
+$conn = getSQLConnectionFromConfig();
+
+$isLeader = isHeadOfClub($username, $clubname, $conn);
+
+if(!$isLeader) {
+    header("Location: index.php");
+    $conn->close();
+    exit();
+}
+
+$events = getClubEvents($clubname, $conn);
+?>
+<table border="1">
+<tr>
+<th>Title</th>
+<th>Location</th>
+<th>Date</th>
+<th>Time</th>
+<th>RSVP Yes %</th>
+<th>Delete</th>
+</tr>
+<?
+foreach($events as $event) {
+    $splits = explode(" at ", $event['description']);
+    $eventName = $splits[0];
+    $eventTime = $splits[1];
+    $eventDate = explode(" ", $event['date'])[0];
+    $eventLoc = $event['location'];
+    $rsvpCount = $event['rsvpCount'];
+    $memberCount = $event['memberCount'];
+    $percentRSVP = bcdiv(intval($rsvpCount), intval($memberCount), 3) * 100;
+?>
+    <tr><td><input type="text" value='<?php echo $eventName; ?>'></td>
+        <td><input type="text" value='<?php echo $eventLoc; ?>'></td>
+        <td><input type="date" value='<?php echo $eventDate; ?>'></td>
+        <td><input type="text" value='<?php echo $eventTime; ?>'></td>
+        <td><?php echo $percentRSVP . "%"; ?></td>
+        <td><input class='X_button' type='button' Value='X'></td>
+    </tr>
+<?php } ?>
+</table>
+<div id = "event_text_line">
+   <p id = "mission_text">Planned Events:</p>
+   <table>
+       <tr>
+         <td> <form class="event_form" >Title:
+           <input id="event_title" class="event_box" type="text">
+         </form>
+         </td>
+         <td> <form class="event_form" >Location:
+           <input id="event_loc" class="event_box" type="text">
+         </form>
+         </td>
+         <td> <form class="event_form" >Date:
+           <input id="event_date" class="event_box" type="date">
+         </form>
+         </td>
+         <td> <form class="event_form" >Time:
+           <input id="event_time" class="event_box" type="text">
+         </form>
+         </td>
+         <td> <form>
+           <input id="add_event_button" type="button" Value = "Add">
+         </form>
+         </td>
+       </tr>
+   </table>
+<? $conn->close(); ?>
---
+++ club_edit_feed.php
@@ -0,0 +1,60 @@
+<?php session_start(); ?>
+<?php
+if(!isset($_SESSION['user']) && !isset($_GET['club'])) { //Need to be authenticated to get to this page
+    header("Location: index.php");
+    exit();
+}
+
+$clubname = $_GET['club'];
+$username = $_SESSION['user'];
+
+require 'scripts/SQLUtils.php';
+require 'scripts/index_utils.php';
+require 'scripts/club_utils.php';
+
+$conn = getSQLConnectionFromConfig();
+
+$isLeader = isHeadOfClub($username, $clubname, $conn);
+
+if(!$isLeader) {
+    header("Location: index.php");
+    $conn->close();
+    exit();
+}
+
+$posts = getClubFeedPosts($clubname, $conn);
+?>
+<div class="add_club_feed_event text_line">
+    <form>
+        Post Content:
+        <textarea>
+        </textarea>
+        <input type="button" value="Post">
+    </form>
+</div>
+<div class="club_events">
+    <h3>Posts:</h3>
+    <table border="1">
+        <tr>
+        <th>Poster</th>
+        <th>Date Posted</th>
+        <th>Content</th>
+        <th>Delete</th>
+        </tr>
+        <?php
+        foreach($posts as $post) {
+            $poster = $post['poster'];
+            $datePosted = $post['dateCreated'];
+            $content = $post['content'];
+        ?>
+        <tr>
+            <td><?php echo $poster; ?></td>
+            <td><?php echo $datePosted; ?></td>
+            <td><textarea><?php echo $content; ?></textarea></td>
+            <td><input class="X_button" type="button" value="X"></td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+</div>
