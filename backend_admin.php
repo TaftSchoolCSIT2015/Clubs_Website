e8920d0209cf354b932f7361fdf7da5a1d891507
Becker Ewing <jbewing@taftschool.org>
2015-10-29 00:23:06 -0400

    Started Backend-Admin Page

--- backend_admin.php
+++ backend_admin.php
@@ -11,3 +11,27 @@
         exit();
     }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Backend Administration</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style/common.css">
+        <link rel="stylesheet" type="text/css" href="backend_admin.css">
+        <script src="js/jquery-2.1.4.min.js"></script>
+    </head>
+    <body>
+        <div class="header">
+            <div class="title">
+                <span>Backend Administration Page</span>
+            </div>
+            <div class="nav">
+                <ul>
+                    <li>Test</li>
+                </ul>
+            </div>
+        </div>
+        <script type="text/javascript" src="js/common.js"></script>
+    </body>
+</html>
